b93ab5a5fc1181ce1e629cd65506b5c724d1a6b4
Abdi Farah <abdirahman.z.farah@gmail.com>
2020-06-29 18:50:03 -0700

    Pig game

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,21 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 1;
+
+dice = Math.floor(Math.random() * 6) + 1;
+// console.log(dice);
+
+// Setter
+document.querySelector("#current-" + activePlayer).textContent = dice;
+
+// Getter
+var x = document.querySelector("#score-0").textContent;
+console.log(x);
+
+document.querySelector(".dice").style.display = "none";
